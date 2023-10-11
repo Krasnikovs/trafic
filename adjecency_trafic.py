7b91d1b491c14e3c40cf61f6326e8d31c65f90ba
Krasnikovs <ralis3333@gmail.com>
2023-10-11 13:29:20 +0300

    Add files via upload

--- adjecency_trafic.py
+++ adjecency_trafic.py
@@ -1,41 +1,91 @@
+from typing import Optional
 import numpy as np
 import random as ran
 import time
 import matplotlib.pyplot as plt
 
+class Vehicle():
+    def __init__(
+        self,
+        name: str,
+        vehicle_vctr: np.array,
+        inArea: Optional[bool] = None,
+        round: Optional[int] = None
+    ):
+        self.name = name
+        self.vehicle_vctr = vehicle_vctr    
+        self.inArea = inArea
+        self.round = round
+    
+    def info(self):
+        print(self.name, self.vehicle_vctr, self.inArea, self.round)
 
 class AdjTrafic():
-    def __init__(self):
-        return
+    def __init__(
+            self, 
+            vehicle_space: Optional[bool] = True,
+    ):
+        self.vehicle_space = vehicle_space
     
-    def inital_position(self):
-        listA = [12, 38, 43]
-        self.vctrA = np.array(listA)
-        self.vctrExists = True
+    def inital_setup(self):
+        self.corners = [[0 for x in range(3)] for y in range(5)]
+        self.vehicles = [None for x in range(10)]
 
-        self.corners = [None, None, None, None, None]
-        self.current_corner = ran.randint(0, 4)
-        self.corners[self.current_corner] = self.vctrA
-        print("Inital position:", self.corners)
+    def create_vehicle(self, cycle):
+        self.vehicle_vctr = [ran.randint(0, 128), ran.randint(0, 128)]
+        vehicle_name = "aa" + str(self.vehicle_vctr[0])
+        print(cycle)
+        self.vehicles[cycle] = Vehicle(
+            name = vehicle_name,
+            vehicle_vctr = self.vehicle_vctr,
+            inArea = True,
+            round = cycle,
+        )
 
+        self.position(cycle)
 
-    def position_change(self):
-        self.new_corner = ran.randint(0, 5)
-        if self.new_corner != 5 and self.new_corner != self.current_corner:
-            self.travel_time()
-            self.corners[self.current_corner] = None
-            self.current_corner = self.new_corner
-            self.corners[self.current_corner] = self.vctrA
-        elif self.new_corner == self.current_corner:
-            print("slow")
-            self.position_change()
+    def position(self, cycle):
+        new_corner = ran.randint(0, 4)
+        
+        if self.corners[new_corner][0] == 0:
+            self.corners[new_corner][0] = self.vehicles[cycle]
+            self.vehicles[cycle].info()
+            print('New corner:', new_corner, 0)
+            return
+        elif self.corners[new_corner][1] == 0:
+            self.corners[new_corner][1] = self.vehicles[cycle]
+            self.vehicles[cycle].info()
+            print('New corner:', new_corner, 1)
+            return
+        elif self.corners[new_corner][2] == 0:
+            self.corners[new_corner][2] = self.vehicles[cycle]
+            self.vehicles[cycle].info()
+            print('New corner:', new_corner, 2)
             return
         else:
-            print("its gone")
-            self.vctrExists = False
-            self.corners[self.current_corner] = None
-        print(self.corners)
+            print('tt')
+            return
+        
+    
+    def change_corner(self, cycle1):
+        new_corner = ran.randint(0, 5)
 
+        if cycle1 > 0:
+            if new_corner == 5:
+                self.vehicle_space = False
+                self.vehicles[cycle] = None
+                return
+            elif self.corners[new_corner][0] == 0:
+                self.corners[new_corner][0] = self.vehicles[cycle]
+                return
+            elif self.corners[new_corner][1] == 0:
+                self.corners[new_corner][1] = self.vehicles[cycle]
+                return
+            elif self.corners[new_corner][2] == 0:
+                self.corners[new_corner][2] = self.vehicles[cycle]
+                return
+            else:
+                return
 
     def travel_time(self):
         self.time_table = np.array([
@@ -67,10 +117,10 @@ class AdjTrafic():
         for i in range(self.turn - 1):
             self.recording[i] = self.rec[i]
         
-        if self.new_corner == 5 and self.turn == 1:
-            return
-        else:
-            self.lgnorm()
+        # if self.new_corner == 5 and self.turn == 1:
+        #     return
+        # else:
+        #     self.lgnorm()
 
 
     def lgnorm(self):
@@ -78,19 +128,56 @@ class AdjTrafic():
         aaa = [0] * (len(self.recording))
         for i in range(len(self.recording)):
             aaa[i] = (self.recording[i] - mean)**2
-        sigma = np.sqrt(sum(aaa) / (len(aaa) - 1))
-        s = np.random.lognormal(mean, sigma, 1000)
+        sigma = np.sqrt(sum(aaa) / (len(aaa)))
+        # s = np.random.lognormal(mean, sigma, 1000)
 
-        count, bins, ignored = plt.hist(s, 100, density=True, align="mid")
+        # count, bins, ignored = plt.hist(s, 100, density=True, align="mid")
 
-        x = np.linspace(min(bins), max(bins), 10000)
-        pdf = (np.exp(-(np.log(x) - mean)**2 / (2 * sigma**2))
-            / (x * sigma * np.sqrt(2 * np.pi)))
+        # x = np.linspace(min(bins), max(bins), 10000)
+        # pdf = (np.exp(-(np.log(x) - mean)**2 / (2 * sigma**2))
+        #     / (x * sigma * np.sqrt(2 * np.pi)))
+
+        
 
         plt.plot(x, pdf, linewidth=2, color="r")
         plt.axis("tight")
         plt.show()
 
+    def check_space(self):
+        print('checking space', cycle)
+        if self.vehicles[cycle] != None:
+            if self.vehicles[cycle].inArea == False:
+                self.vehicles[cycle] = None
+                self.vehicle_space = True
+                print('free space')
+                return self.vehicle_space
+            elif self.vehicles[cycle].inArea == True:
+                self.vehicle_space = False
+                print('change to false')
+                return self.vehicle_space
+        elif self.vehicles[cycle] == None:
+            self.vehicle_space = True
+            print('free space -')
+            return self.vehicle_space
+        else:
+            print('sad')
 
-trafic = AdjTrafic()
-trafic.loop()
+trafic = AdjTrafic()    
+
+cycle = 0
+cycle1 = 0
+trafic.inital_setup()
+while cycle < 10:
+    print('Cycle:', cycle)
+    if trafic.vehicle_space == True:
+        print('True')
+        trafic.create_vehicle(cycle)
+    else:
+        print('False')
+    trafic.change_corner(cycle1)
+    trafic.check_space()
+    cycle += 1
+    if cycle == 10:
+        cycle = 0
+        cycle1 += 1
+    time.sleep(1)
