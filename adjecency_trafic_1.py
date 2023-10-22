f72aaacc273a0d0bf6e1d59919bea87670100b58
GoodChair <ralis3333@gmail.com>
2023-10-22 17:18:08 +0300

    something

--- adjecency_trafic_1.py
+++ adjecency_trafic_1.py
@@ -1,8 +1,6 @@
 from typing import Optional
 import numpy as np
 import random as ran
-import time
-import matplotlib.pyplot as plt
 
 class Vehicle():
     def __init__(
@@ -17,13 +15,33 @@ class Vehicle():
         self.lifetime : int = self.random_lognorm(sigma, mu)
 
     def step(self, graph):
+        for i in range(graph):
+            if self.vehicle_vctr == graph[i]:
+                self.postion = graph[i]
+        space = np.array
+        space_c = 0
+        for i in range(graph - 1):
+            for e in range(graph[i]):
+                if graph[i][e] == 0:
+                    space[space_c] = graph[i]
+                    space_c += 1
+                    return
+        
+        next_position = ran.random(len(space))
+
         pass
 
-    def random_lognormal(self):
-        pass
+    def random_lognormal(self, mu, sigma):
+        x = ran.random(1, 10)
+        e = 2.71828
+        Z = (x - mu) / sigma
+        X = (e**mu)*((e**Z)**sigma)
+        return X
 
     def get_vehicles(self):
         pass
+
+graph = [v1, v2, v3, v4, v5, r1]
     
 def create_vehicles(amount):
     for amount in range(amount):
@@ -32,26 +50,30 @@ def create_vehicles(amount):
             position = ran.randint(0, 5),
         )
 
+car = Vehicle()
 
 for episodes in range(episodes):
     create_vehicles()
-    for car in cars:
+    for car in range(car):
         car.step(graph)
         # 
 
 
 
 
-    def travel_time(self):
-        self.time_table = np.array([
-            [0, 2, 5, 8, 7],
-            [2, 0, 6, 9, 4],
-            [5, 6, 0, 8, 7],
-            [8, 9, 8, 0, 2],
-            [7, 4, 7, 2, 0],
-        ])
+
+
+def travel_time(self):
+    self.time_table = np.array([
+        [0, 2, 5, 8, 7],
+        [2, 0, 6, 9, 4],
+        [5, 6, 0, 8, 7],
+        [8, 9, 8, 0, 2],
+        [7, 4, 7, 2, 0],
+    ])
         
-       
+def get_cars():
+    pass
 
 trafic = AdjTrafic()
 
@@ -76,5 +98,3 @@ while True:
             
             trafic.lgnorm_dis()
             break
-    # trafic.check()
-    #time.sleep(1)
